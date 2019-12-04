ee420c7963db1244ebdf95017a4dae071a8dce2c
Ubuntu <ubuntu@ip-172-31-44-235.us-east-2.compute.internal>
2019-12-03 22:59:24 -0500

    fix scripts

--- scripts/downloadData.sh
+++ scripts/downloadData.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/ubuntu/Lean/Launcher/bin/Debug
 NOW=$(date +"%Y%m%d-00:00:00")
-mono QuantConnect.ToolBox.exe --app=YahooDownloader --tickers=SPY,TLT --resolution=Daily --from-date=20030502-00:00:00 --to-date=$NOW
+mono QuantConnect.ToolBox.exe --app=YahooDownloader --tickers=SPY,TLT --resolution=Daily --from-date=20190902-00:00:00 --to-date=$NOW

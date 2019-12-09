245d9b8df55ea1037aa3c3b601e13f96dc539031
dennis <dennis@denniss-MacBook-Pro.local>
2019-12-09 15:08:26 -0500

    roll back to working algorithm code

--- Algorithm.CSharp/PairsTradingAlgorithm.cs
+++ Algorithm.CSharp/PairsTradingAlgorithm.cs
@@ -2,9 +2,21 @@
  * Copyright Chris Short 2019
 */
 
+using System;
+using System.Collections.Generic;
+using QuantConnect.Algorithm.Framework.Alphas;
+using QuantConnect.Algorithm.Framework.Execution;
+using QuantConnect.Algorithm.Framework.Portfolio;
+using QuantConnect.Algorithm.Framework.Risk;
+using QuantConnect.Algorithm.Framework.Selection;
+using QuantConnect.Orders;
+using QuantConnect.Interfaces;
+
 using QuantConnect.Data;
 using QuantConnect.Data.Market;
+using QuantConnect.Indicators;
 using System.Linq;
+using System.Threading;
 using QuantConnect.Orders.Slippage;
 
 namespace QuantConnect.Algorithm.CSharp
@@ -17,79 +29,33 @@ namespace QuantConnect.Algorithm.CSharp
     /// <meta name="tag" content="trading and orders" />
     public class PairsTradingAlgorithm : QCAlgorithm
     {
-        // TODO: test adding SHY to universe (short term bond for inverted yield curve scenario)
-        // TODO: find a way to avoid high momentum due to big temporary drop 60 days ago (if that matters)
-
-        private static readonly int slowDays = 60;
-        private static readonly decimal flipMargin = 0.035m;
-        private string symbolInMarket = string.Empty;
-        private readonly ISlippageModel SlippageModel = new ConstantSlippageModel(0.002m);
-
+        /// <summary>
+        /// Initialise the data and resolution required, as well as the cash and start-end dates for your algorithm. All algorithms must initialized.
+        /// </summary>
         public override void Initialize()
         {
-            SetStartDate(2003, 8, 4);
-            SetEndDate(2019, 8, 30);
-            SetCash(100000);
-            var spy = AddEquity("SPY", Resolution.Daily);
-            spy.SetSlippageModel(SlippageModel);
-            var tlt = AddEquity("TLT", Resolution.Daily);
-            tlt.SetSlippageModel(SlippageModel);
+            // Set requested data resolution
+            UniverseSettings.Resolution = Resolution.Minute;
 
-            SetSecurityInitializer(x => x.SetDataNormalizationMode(DataNormalizationMode.Raw));
+            SetStartDate(2013, 10, 07);  //Set Start Date
+            SetEndDate(2013, 10, 11);    //Set End Date
+            SetCash(100000);             //Set Strategy Cash
+
+            var resolution = LiveMode ? Resolution.Minute : Resolution.Daily;
+            AddEquity("SPY", resolution, null, true);
         }
 
         public override void OnData(Slice slice)
         {
+            SetHoldings("SPY", 1m, false);
+        }
 
-            var spyMomentum = Momentum("SPY", slowDays);
-            var tltMomentum = Momentum("TLT", slowDays);
-
-            PlotPoints(spyMomentum, tltMomentum);
-
-            if (spyMomentum > tltMomentum + flipMargin)
+        public override void OnOrderEvent(OrderEvent orderEvent)
+        {
+            if (orderEvent.Status.IsFill())
             {
-                Rebalance("SPY", "TLT");
+                Debug($"Purchased Stock: {orderEvent.Symbol}");
             }
-            else if (tltMomentum > spyMomentum + flipMargin)
-            {
-                Rebalance("TLT", "SPY");
-            }
-        }
-
-        private void PlotPoints(decimal spyMomentum, decimal tltMomentum)
-        {
-            Plot("momentum", "spyMomentum", (spyMomentum - 1) * 1);
-            Plot("momentum", "tltMomentum", (tltMomentum - 1) * 1);
-
-            Plot("price", "SPY", Securities["SPY"].Price);
-            Plot("price", "tlt", Securities["TLT"].Price);
-
-            Plot("leverage", "cash", Portfolio.Cash);
-            Plot("leverage", "holdings", Portfolio.TotalHoldingsValue);
-        }
-
-        private void Rebalance(string buySymbol, string sellSymbol)
-        {
-
-            if (buySymbol == symbolInMarket && Portfolio.Cash < Portfolio.TotalHoldingsValue)
-                return;
-
-            Liquidate(sellSymbol);
-            SetHoldings(buySymbol, 1m, false);
-            symbolInMarket = buySymbol;
-        }
-
-        private int sharesToBuy(string symbol)
-        {
-            var value = Portfolio.Cash + Portfolio.TotalHoldingsValue;
-            return (int)(value / Securities[symbol].Price);
-        }
-
-        private decimal Momentum(string symbol, int days)
-        {
-            var h = History<TradeBar>(symbol, days);
-            //Debug($"{symbol} momentum = {Securities[symbol].Price} / {h.First().Close}");
-            return Securities[symbol].Price / h.First().Close;
         }
     }
 }

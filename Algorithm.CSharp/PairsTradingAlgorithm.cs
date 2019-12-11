f615b62e426650c278abc7a6819353ce969cf87d
dennis <dennis@denniss-MacBook-Pro.local>
2019-12-11 13:32:39 -0500

    slowly building working algo

--- Algorithm.CSharp/PairsTradingAlgorithm.cs
+++ Algorithm.CSharp/PairsTradingAlgorithm.cs
@@ -29,9 +29,15 @@ namespace QuantConnect.Algorithm.CSharp
     /// <meta name="tag" content="trading and orders" />
     public class PairsTradingAlgorithm : QCAlgorithm
     {
-        /// <summary>
-        /// Initialise the data and resolution required, as well as the cash and start-end dates for your algorithm. All algorithms must initialized.
-        /// </summary>
+        // TODO: test adding SHY to universe (short term bond for inverted yield curve scenario)
+        // TODO: find a way to avoid high momentum due to big temporary drop 60 days ago (if that matters)
+        // TODO: use trading day's open price as momenum numerator
+
+        private static readonly int slowDays = 60;
+        private static readonly decimal flipMargin = 0.035m;
+        private string symbolInMarket = string.Empty;
+        private readonly ISlippageModel SlippageModel = new ConstantSlippageModel(0.002m);
+
         public override void Initialize()
         {
             // Set requested data resolution
@@ -50,12 +56,40 @@ namespace QuantConnect.Algorithm.CSharp
             SetHoldings("SPY", 1m, false);
         }
 
-        public override void OnOrderEvent(OrderEvent orderEvent)
+        private void PlotPoints(decimal spyMomentum, decimal tltMomentum)
         {
-            if (orderEvent.Status.IsFill())
-            {
-                Debug($"Purchased Stock: {orderEvent.Symbol}");
-            }
+            Plot("momentum", "spyMomentum", (spyMomentum - 1) * 1);
+            Plot("momentum", "tltMomentum", (tltMomentum - 1) * 1);
+
+            Plot("price", "SPY", Securities["SPY"].Price);
+            Plot("price", "tlt", Securities["TLT"].Price);
+
+            Plot("leverage", "cash", Portfolio.Cash);
+            Plot("leverage", "holdings", Portfolio.TotalHoldingsValue);
+        }
+
+        private void Rebalance(string buySymbol, string sellSymbol)
+        {
+
+            if (buySymbol == symbolInMarket && Portfolio.Cash < Portfolio.TotalHoldingsValue)
+                return;
+
+            Liquidate(sellSymbol);
+            SetHoldings(buySymbol, 1m, false);
+            symbolInMarket = buySymbol;
+        }
+
+        private int sharesToBuy(string symbol)
+        {
+            var value = Portfolio.Cash + Portfolio.TotalHoldingsValue;
+            return (int)(value / Securities[symbol].Price);
+        }
+
+        private decimal Momentum(string symbol, int days)
+        {
+            var h = History<TradeBar>(symbol, days);
+            //Debug($"{symbol} momentum = {Securities[symbol].Price} / {h.First().Close}");
+            return Securities[symbol].Price / h.First().Close;
         }
     }
 }

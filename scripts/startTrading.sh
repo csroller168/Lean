1a19397444d646f3edc93afe3be96c1953c6ccae
Ubuntu <ubuntu@ip-172-31-17-250.us-east-2.compute.internal>
2020-04-24 01:59:07 +0000

    update scripts

--- scripts/startTrading.sh
+++ scripts/startTrading.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/ubuntu/git/Lean/Launcher/bin/Debug
-mono QuantConnect.Lean.Launcher.exe &> /home/ubuntu/tradeLog.txt
+mono QuantConnect.Lean.Launcher.exe &> /home/ubuntu/logs/tradeLog.txt

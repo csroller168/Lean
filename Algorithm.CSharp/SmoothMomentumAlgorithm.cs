360398077f9bfcdfdbe6ba20218529fbf4f6860c
csroller168 <chrisshort168@gmail.com>
2021-02-17 16:06:21 -0500

    todos

--- Algorithm.CSharp/SmoothMomentumAlgorithm.cs
+++ Algorithm.CSharp/SmoothMomentumAlgorithm.cs
@@ -28,14 +28,12 @@ namespace QuantConnect.Algorithm.CSharp
         //      submit alpha when done (https://www.youtube.com/watch?v=f1F4q4KsmAY)
         //
         // short-term TODOs:
-        //      rank, and set sell threshold by rank
         //      test hedge with shorts
         //      test hedge with vix
         //      test only sell with SPY > SMA(100)
         //      test more universe filters (market cap, dollar volume, volatility, etc.)
         //      open only when vix momentum < 2
         //      only open when most stocks >= SMA(100)
-        //      only buy once per week
         
         private static readonly string[] ExchangesAllowed = { "NYS", "NAS" };
         private static readonly string AllowedCountry = "USA";
